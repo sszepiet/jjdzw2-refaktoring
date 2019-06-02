ee142b4b2a303f15a0f6642f9c75ed12ef7911bb
Szymon Szepietowski <sszepiet@gmail.com>
2019-06-02 08:56:45 +0200

    Workshop 4: extracted methods

--- src/main/java/pl/infoshare/acedemy/workshop4/App.java
+++ src/main/java/pl/infoshare/acedemy/workshop4/App.java
@@ -6,21 +6,30 @@ public class App {
     private static final Company company = new Company(INFOSHARE);
 
     public static void main(final String[] args) {
+        hireEmployees();
+        listEmployees();
+    }
 
-        final Employee cto = new Employee(Employee.TITLE_CTO);
-        final Employee cfo = new Employee(Employee.TITLE_CFO);
-        final Employee coo = new Employee(Employee.TITLE_COO);
+    private static void hireEmployees() {
+        final Employee cto = prepareEmployee(Employee.TITLE_CTO);
+        final Employee cfo = prepareEmployee(Employee.TITLE_CFO);
+        final Employee coo = prepareEmployee(Employee.TITLE_COO);
 
         company.employees.add(cto);
         company.employees.add(cfo);
         company.employees.add(coo);
+    }
 
+    private static Employee prepareEmployee(String title) {
+        return new Employee(title);
+    }
+
+    private static void listEmployees() {
         System.out.println("--- " + company.name + " employees ---");
         for (int i = 0; i < company.employees.size(); i++) {
             final int ordinal = i + 1;
             System.out.println(ordinal + ". " + company.employees.get(i).title);
         }
-
     }
 
 }
