efc446d113ea37e5106b44d41fb7c73231ba66fb
Szymon Szepietowski <sszepiet@gmail.com>
2019-06-02 08:44:02 +0200

    Workshop 4 created

---
+++ src/main/java/pl/infoshare/acedemy/workshop4/App.java
@@ -0,0 +1,26 @@
+package pl.infoshare.acedemy.workshop4;
+
+public class App {
+
+    private static final String INFOSHARE = "infoShare Academy";
+    private static final Company company = new Company(INFOSHARE);
+
+    public static void main(final String[] args) {
+
+        final Employee cto = new Employee(Employee.TITLE_CTO);
+        final Employee cfo = new Employee(Employee.TITLE_CFO);
+        final Employee coo = new Employee(Employee.TITLE_COO);
+
+        company.employees.add(cto);
+        company.employees.add(cfo);
+        company.employees.add(coo);
+
+        System.out.println("--- " + company.name + " employees ---");
+        for (int i = 0; i < company.employees.size(); i++) {
+            final int ordinal = i + 1;
+            System.out.println(ordinal + ". " + company.employees.get(i).title);
+        }
+
+    }
+
+}
