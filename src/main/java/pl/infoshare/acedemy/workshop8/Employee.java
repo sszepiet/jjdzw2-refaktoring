521ae4da35ae9eb2de44f78644d28c2ed21c139f
Szymon Szepietowski <sszepiet@gmail.com>
2019-06-02 09:35:14 +0200

    Workshop 8: created

---
+++ src/main/java/pl/infoshare/acedemy/workshop8/Employee.java
@@ -0,0 +1,13 @@
+package pl.infoshare.acedemy.workshop8;
+
+public class Employee {
+
+    static final String TITLE_CTO = "CTO";
+    static final String TITLE_CFO = "CFO";
+    static final String TITLE_COO = "COO";
+    String title;
+
+    public Employee(String title) {
+        this.title = title;
+    }
+}
