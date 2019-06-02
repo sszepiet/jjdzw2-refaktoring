ac10016eb10282495b535cdc9d4cdce472e5cdd2
Szymon Szepietowski <sszepiet@gmail.com>
2019-06-02 09:28:14 +0200

    Workshop 7: created

---
+++ src/main/java/pl/infoshare/acedemy/workshop7/App.java
@@ -0,0 +1,35 @@
+package pl.infoshare.acedemy.workshop7;
+
+public class App {
+
+    private static final String INFOSHARE = "infoShare Academy";
+    private static final Company company = new Company(INFOSHARE);
+
+    public static void main(final String[] args) {
+        hireEmployees(company);
+        listEmployees(company);
+    }
+
+    private static void hireEmployees(Company company) {
+        final Employee cto = prepareEmployee(Employee.TITLE_CTO);
+        final Employee cfo = prepareEmployee(Employee.TITLE_CFO);
+        final Employee coo = prepareEmployee(Employee.TITLE_COO);
+
+        company.employees.add(cto);
+        company.employees.add(cfo);
+        company.employees.add(coo);
+    }
+
+    private static Employee prepareEmployee(String title) {
+        return new Employee(title);
+    }
+
+    private static void listEmployees(Company company) {
+        System.out.println("--- " + company.name + " employees ---");
+        for (int i = 0; i < company.employees.size(); i++) {
+            final int ordinal = i + 1;
+            System.out.println(ordinal + ". " + company.employees.get(i).title);
+        }
+    }
+
+}
---
+++ src/main/java/pl/infoshare/acedemy/workshop7/Company.java
@@ -0,0 +1,15 @@
+package pl.infoshare.acedemy.workshop7;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Company {
+
+    String name;
+    List<Employee> employees;
+
+    public Company(String name) {
+        this.name = name;
+        this.employees = new ArrayList<>();
+    }
+}
